dba54d503cddb911d23c7a8a0044850efb3ce3b8
Tim Van Meerbeeck <Tim.VanMeerbeeck@dxcfds.com>
2020-10-16 13:22:36 +0200

    multi ling support no longer needed + added the default belgiche vrouw

--- src/main/java/be/vlaanderen/burgerloket/attestdemo/groenestroomcertificaat/config/DatabaseLoader.java
+++ src/main/java/be/vlaanderen/burgerloket/attestdemo/groenestroomcertificaat/config/DatabaseLoader.java
@@ -13,14 +13,19 @@ public class DatabaseLoader {
     CommandLineRunner init(GroeneStroomCertficaatRepository repository) {
 
         return args -> {
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 5; i++) {
                 repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "83020711970", String.valueOf(2020-i), "nl"));
-                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "83020711970", String.valueOf(2020-i), "fr"));
+//                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "83020711970", String.valueOf(2020-i), "fr"));
 //                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "83020711970", String.valueOf(2020-i), "en"));
 //                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "83020711970", String.valueOf(2020-i), "de"));
 
                 repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "nl"));
-                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "fr"));
+//                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "fr"));
+//                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "en"));
+//                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "de"));
+
+                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "71090999892", String.valueOf(2020-i), "nl"));
+//                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "fr"));
 //                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "en"));
 //                repository.save(new GroeneStroomCertificaat("Groene stroom certificaat " + (2020 - i), "78122248790", String.valueOf(2020-i), "de"));
             }
